add83b8bfd84fad279d51a68b313c0f74b9a75b8
Rayhan Auliarahman <raauliarahman@gmail.com>
2026-06-10 20:43:34 +0700

    chore: configure PostgreSQL for Supabase

--- prisma/migrations/20260610134323_init_postgres/migration.sql
+++ prisma/migrations/20260610134323_init_postgres/migration.sql
@@ -1,13 +1,15 @@
 -- CreateTable
 CREATE TABLE "schedules" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "class_code" TEXT NOT NULL,
     "class_name" TEXT NOT NULL,
     "subject_code" TEXT NOT NULL,
     "teacher_nik" TEXT NOT NULL,
     "teacher_name" TEXT NOT NULL,
-    "date" DATETIME NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
     "jam_ke" INTEGER NOT NULL,
     "time_start" TEXT NOT NULL,
-    "time_end" TEXT NOT NULL
+    "time_end" TEXT NOT NULL,
+
+    CONSTRAINT "schedules_pkey" PRIMARY KEY ("id")
 );
